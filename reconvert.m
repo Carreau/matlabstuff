11d08c885e5575cc28204b89b225b29a0aa5908d
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2011-01-24 15:31:36 +0100

    convert to char

--- reconvert.m
+++ reconvert.m
@@ -7,7 +7,7 @@ for i=1:length(names)
     nm = char(names(i));
     f = load(nm);
     newname = [nm(1:end-4) '_matthias.mat']
-    save('newname','-struct','f', 'bead_radius', 'want_filter', 'f_3dB_filter', 'rate', 'T', 'l', 'f_lim_up', 'f_lim_low', 'nb', 'slopes', 'Traps', 'remove_peaks', 'eta_x', 'kappa_x', 'gamma_x', 'D_x', 'eta_y', 'kappa_y', 'gamma_y', 'D_y');
+    save(char(newname),'-struct','f', 'bead_radius', 'want_filter', 'f_3dB_filter', 'rate', 'T', 'l', 'f_lim_up', 'f_lim_low', 'nb', 'slopes', 'Traps', 'remove_peaks', 'eta_x', 'kappa_x', 'gamma_x', 'D_x', 'eta_y', 'kappa_y', 'gamma_y', 'D_y');
     clear f;
 end
 
