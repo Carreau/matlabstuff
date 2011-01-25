f0d7b5471174c5a53029fb9d78db117b6f73bdfd
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2011-01-25 17:34:07 +0100

    subroutine to merge results ans psd files

---
+++ loadfunction.m
@@ -0,0 +1,8 @@
+% just a dummy loadfunction which look like doing the same as load('smth')
+% but which cn be used in structfun or cellfun.
+
+function [structhandle] = loadfunction(path)
+	path
+	structhandle = load(path);
+end
+	
---
+++ mergeResultFiles.m
@@ -0,0 +1,25 @@
+% function to merge 2 types of files, 
+% calibration_simons10XX_AOM=0_bead=N_results.mat
+% and
+% calibration_simons10XX_AOM=0_bead=N_psd.mat
+% use a patern matching % calibration_simons10XX_AOM=0_bead=N_results.mat ('*results.mat')
+% and it will look for the psd corresponding file and merge the 2 in a '..merged_v01.mat' file
+
+function mergeResultsFiles(patern)
+	d = dir(patern);
+	l= length('results.mat');
+	names_orig		= arrayfun(@(x) x.name			,d			,'UniformOutput',false);
+	base_names		= cellfun( @(x) (x(1:end-l))	,names_orig	,'UniformOutput',false)
+	names_extended	= cellfun( @(x) [x 'psd.mat']	,base_names	,'UniformOutput',false)
+	names_newoutput	= cellfun( @(x) [x 'merged_v01.mat'],base_names	,'UniformOutput',false)
+	cellfun(@(x,y,z) actualmerge(x,y,z),names_orig,names_extended,names_newoutput)
+end
+
+
+function actualmerge(resultfilename,extendedfilname,outputfilename)
+	x = loadfunction(resultfilename);
+	y = loadfunction(extendedfilname);
+	c = catstruct(x,y,'sorted');
+	save(outputfilename,'-struct','c');
+end
+
