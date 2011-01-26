9dc5489342dcf532eb8b2c467d5b2e6ca03ee0fa
unknown <labo@.(none)>
2011-01-26 13:15:47 +0100

    make merge rsult silent

--- mergeResultFiles.m
+++ mergeResultFiles.m
@@ -9,10 +9,10 @@ function mergeResultsFiles(patern)
 	d = dir(patern);
 	l= length('results.mat');
 	names_orig		= arrayfun(@(x) x.name			,d			,'UniformOutput',false);
-	base_names		= cellfun( @(x) (x(1:end-l))	,names_orig	,'UniformOutput',false)
-	names_extended	= cellfun( @(x) [x 'psd.mat']	,base_names	,'UniformOutput',false)
-	names_newoutput	= cellfun( @(x) [x 'merged_v01.mat'],base_names	,'UniformOutput',false)
-	cellfun(@(x,y,z) actualmerge(x,y,z),names_orig,names_extended,names_newoutput)
+	base_names		= cellfun( @(x) (x(1:end-l))	,names_orig	,'UniformOutput',false);
+	names_extended	= cellfun( @(x) [x 'psd.mat']	,base_names	,'UniformOutput',false);
+	names_newoutput	= cellfun( @(x) [x 'merged_v01.mat'],base_names	,'UniformOutput',false);
+	cellfun(@(x,y,z) actualmerge(x,y,z),names_orig,names_extended,names_newoutput);
 end
 
 
