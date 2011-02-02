efebf564f9b951e16211add9ea70a94cbd3b1467
Bussonnier Matthias <bussonniermatthias@gmail.com>
2011-02-02 16:19:30 +0100

    live as struct

--- liveviscosity.m
+++ liveviscosity.m
@@ -1,10 +1,9 @@
 function [files] =  liveViscosity()
 	recursiveMergeResults();
-	files =loadFilesAsCellArray('.','_v01.mat');
-	files = structFilterdByFieldValue(files,'eta_x',@(x) x < 0.10);
-	etax = cellfun(@(x) x.eta_x, files);
-	etay = cellfun(@(x) x.eta_y, files);
-	time = cellfun(@(x) datenum(x.timestamp),files);
+	files =loadFilesAsStructArray('.','_v01.mat');
+	etax = arrayfun(@(x) x.eta_x, files);
+	etay = arrayfun(@(x) x.eta_y, files);
+	time = arrayfun(@(x) datenum(x.timestamp),files);
 	figure(1);
 	plot(etax,etay,'+');
 	hold on;
