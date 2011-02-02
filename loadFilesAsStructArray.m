dd49ffc5545440728577ad4d8916edc2dad956d4
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2011-02-02 11:45:58 +0100

    switching to struct

---
+++ loadFilesAsStructArray.m
@@ -0,0 +1,10 @@
+function [files] = loadFilesAsStructArray(path,patern)
+
+[f,bytes,path]=  dirr(path,patern,'name');
+fileList = arrayfun(@(x) load(char(x)),path,'UniformOutput',false)
+files = cellfun(@(x) x,fileList)
+
+%cellfun(@(x) isfield(x,'eta_x'),file)
+%arrayfun(@(x) load(char(x)),p)
+
+
