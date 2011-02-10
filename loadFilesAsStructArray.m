4a9a5ea1830dc0daa132c76a67fc99857f129a07
Bussonnier Matthias <bussonniermatthias@gmail.com>
2011-02-10 15:21:45 +0100

    silece things

--- loadFilesAsStructArray.m
+++ loadFilesAsStructArray.m
@@ -1,8 +1,8 @@
 function [files] = loadFilesAsStructArray(path,patern)
 
 [f,bytes,path]=  dirr(path,patern,'name');
-fileList = arrayfun(@(x) load(char(x)),path,'UniformOutput',false)
-files = cellfun(@(x) x,fileList)
+fileList = arrayfun(@(x) load(char(x)),path,'UniformOutput',false);
+files = cellfun(@(x) x,fileList);
 
 %cellfun(@(x) isfield(x,'eta_x'),file)
 %arrayfun(@(x) load(char(x)),p)
