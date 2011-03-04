ea4129f99edb7fa5ba8b7e235cab70f1a65a1e86
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2011-03-04 15:33:49 +0100

    fix mistake in trap.m
        remove useless constructor in trap.m en d fix /QPD_sums to ./QPD_sum

--- trap.m
+++ trap.m
@@ -22,27 +22,10 @@ classdef trap < handle
             ret.y = self.pos_aod.y ./ self.aod_microm.y;
         end
         function ret=get.absolute_bead_pos(self)
-            ret.x = self.pos_aod.x./self.aod_microm.x-1./self.slopes.x*self.QPD_dx/self.QPD_sum;
-            ret.y = self.pos_aod.y./self.aod_microm.y-1./self.slopes.y*self.QPD_dy/self.QPD_sum;
+            ret.x = self.pos_aod.x./self.aod_microm.x-1./self.slopes.x*self.QPD_dx./self.QPD_sum;
+            ret.y = self.pos_aod.y./self.aod_microm.y-1./self.slopes.y*self.QPD_dy./self.QPD_sum;
         end
-        function obj=twoBeadApprochExperiment
-            obj.kappa.x       %x et y
-            obj.kappa.y       %x et y
-            obj.pos_aod.x         %x et y
-            obj.pos_aod.y
-            obj.bead_pos.x
-            obj.bead_pos.y
-            obj.slopes.x
-            obj.slopes.y
-            obj.force.x
-            obj.force.y
-            obj.QPD_dx
-            obj.QPD_dy
-            obj.QPD_sum
-            obj.aod_microm.x
-            obj.aod_microm.y
-        end
-        
-    end 
+    end
 
 end
+
