67334cc0f33faab7f33b34d5cfc591723278c2c8
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2011-03-08 13:42:14 +0100

    modify linbin to remove the point at beginnig and not bin if 0 or 1

--- linbin.m
+++ linbin.m
@@ -1,5 +1,11 @@
 function Avg = linbin(data,binsize)
     %%
+    if( binsize == 0 || binsize == 1)
+        Avg= data;
+        disp('not binning');
+        return
+    end
+    %%
     data=squeeze(data);
     l= size(data,2);
     if (l == 1) && (size(data,1) ~= 1);
@@ -7,7 +13,7 @@ function Avg = linbin(data,binsize)
     end
     l= size(data,2);
     nRemove = rem(l,binsize);  %# Find the number of points to remov
-    data = data(1:end-nRemove);        %# Trim points to make an even multiple of 300
+    data = data(1+nRemove:end);        %# Trim points to make an even multiple of 300
     l= size(data,2);
     %%
     Avg = mean(reshape(data,binsize,l/binsize));
