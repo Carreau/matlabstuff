9b43dbaf1411877eebf84eaac03806b0cba7c5e1
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2011-06-24 10:49:17 +0200

    regext ``é"  in xtexlabel

--- xtexlabel.m
+++ xtexlabel.m
@@ -1,4 +1,8 @@
 function xtexlabel(str)
+	str = regexprep(str, 'é', '\''e');
+    str = regexprep(str, 'è', '\`e');
+    str = regexprep(str, 'à', '\`a');
+    str = regexprep(str, '(\\[A-Za-z]+)', '$$1$');
 	set(0,'defaulttextinterpreter','none')
-	xlabel(['$' str '$']);
+	xlabel(str)
 end
