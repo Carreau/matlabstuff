dff92971540a8133aa63a571c1f0c87cc91296f0
Matthias BUSSONNIER <bussonniermatthias@gmail.com>
2011-06-24 10:50:01 +0200

    remove debug string

--- temptest.m
+++ temptest.m
@@ -1,7 +1,6 @@
 function [s]=temptest( str )
     tmp = load(str);
     f  = tmp.save_data;
-    disp('cocuou');
     %disp('********');
     %disp('merci de donner le date et l''heure, ainsi qu''une description');
     %disp('********');
