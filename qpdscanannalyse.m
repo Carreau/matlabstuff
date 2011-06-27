a7150a218389bbddfbe5470cef344dbb3bbe5dd5
unknown <timo@.(none)>
2011-06-27 17:35:27 +0200

    working qpd scan

---
+++ qpdscanannalyse.m
@@ -0,0 +1,68 @@
+%%
+% dans leprogramme d'exemple, lx ly représentent la longueur des scan
+% suivant x et y 
+% tr représente le time ratio
+%%
+%load('test_qpd_rawdata.mat');
+%data=data_r(:,1:end);%%
+%grid_size=20;
+%number_of_point=10;
+%%
+stp = 2/(grid_size-1);
+
+data=data_r(:,1:end);
+xtrap   = data(1,:);
+ytrap   = data(2,:);
+aodpower= data(3,:);
+qpdx    = data(4,:);
+sigmax  = data(5,:);
+qpdy    = data(6,:);
+sigmay  = data(7,:);
+qpdsum  = data(8,:);
+sigmaqpdsum          = data(9,:);
+powerphotodiode      = data(10,:);
+sigmapowerphotodiode = data(11,:);
+
+%dx = qpdx ./ qpdsum;
+%dy = qpdy ./ qpdsum;
+
+
+%mx  = mean(reshape(dx,number_of_point,[]));
+%my  = mean(reshape(dy,number_of_point,[]));
+%ms  = mean(reshape(qpdsum,number_of_point,[]));
+mpd = mean(reshape(powerphotodiode,number_of_point,[]));
+inpower = mpd /max(mpd);
+
+[X Y] = meshgrid(-1:stp:1,-1:stp:1);
+
+% figure(1)
+% surf(X,Y,reshape(mx,grid_size,[]))
+% title('mx qpd fonction de la position' )
+% 
+% figure(2)
+% quiver(X,Y,reshape(mx,grid_size,[]),reshape(my,grid_size,[]))
+% title('drift photodiode fct de la position');
+% 
+% figure(3)
+% surf(X,Y,reshape(my,grid_size,[]))
+% title('my qpd vs position');
+% 
+% figure(4)
+% surf(X,Y,reshape(ms,grid_size,[]))
+% title('évolution de la puissance sur la qpd en fonction de la position');
+% 
+% figure(5)
+% surf(X,Y,reshape(inpower,grid_size,[]))
+% title('évolution de la puissance sur la PHOTODIODE en %');
+
+figure(11)
+Z=reshape(inpower,grid_size,[]);
+%iz=interp2(Z);
+%ix=interp2(X);
+%iy=interp2(Y);
+[C,h] = contour(X,Y,Z,[0.8:0.02:1.0]);
+set(h,'ShowText','on','TextStep',get(h,'LevelStep')*2)
+%colormap cool
+
+%clear mx my dx dy qpdx qpdy qpdsum
+
--- qpdscantest.m
+++ qpdscantest.m
@@ -8,81 +8,37 @@
 % same for cal which is pix to aod
 % in is tha trap array (255) mean trap is active
 
-% /!\ don't know why but aom_s is not used
 %%
-clear
-o=10;
-tr=10;
-d=5;
-scan_width =[1 1 0];
-cal=[380.0000 257.0000 0.0037 -0.0037 0.0265 -0.0268];
-in=[366 283 255 0 0];
+%clear
+%o=10;
+%tr=10;
+%d=5;
+%scan_width =[1 1 0];
+%cal=[380.0000 257.0000 0.0037 -0.0037 0.0265 -0.0268];
+%in=[366 283 255 0 0];
 
-%
+%%
 % on cherche le piège actif
 % pos est donc le numero du piège actif
 save('D:\temp\matthias\test_qpd_parameters')
 pos = find(in(:,3)==255);
 
-% on a donc la posion initiale du piège actif
-%x_o = cal(3)*(in(pos,1)-cal(1));
-%y_o=  cal(4)*(in(pos,2)-cal(2));
-
-
 %ici on a le voltage du piège actif
 aom_t = in(pos,4);
 clear in pos
 
-%ici on calcul le déplacement du piège 
-%dx = cal(5)*d/1000;
-%dy = cal(6)*d/1000;
-%clear cal
-s
-% on calcul le nombre du point du scan dans chaqu direction
-    
-%n_st_x = round(scan_width(1)*1000/d);
-%n_st_y = round(scan_width(2)*1000/d);
-%clear scan_width d
 
-% on calcul les futurs positions du pièges
-%x_s = x_o+[-n_st_x:n_st_x]*dx;
-%y_s = y_o+[-n_st_y:n_st_y]*dy;
-%clear dx dy n_st_x n_st_y
+%step size on the grid
+grid_size=20;
+number_of_point=100;
+stp = 2/(grid_size-1);
+length([-1:stp:1])
 
-% on les intercales de 'zeros' en concatainant verticalemetn, puis reshape
-%x_t(1,:)=x_s;
-%x_t(2:tr+1,:) = x_o*ones(tr,length(x_s));
-%y_t(1,:)=y_s;
-%y_t(2:tr+1,:) = y_o*ones(tr,length(y_s));
-
-%clear x_s y_s tr
-
-%x_i = reshape(x_t,1,size(x_t,1)*size(x_t,2));
-%y_i = reshape(y_t,1,size(y_t,1)*size(y_t,2));
-%clear x_t y_t
-
-% we add 'zeros' at the beggining and the end
-%x_f = [x_i ones(1,length(y_i))*x_o];
-%y_f = [ones(1,length(x_i))*y_o y_i];
-%clear x_i y_i
-
-% and ofset it
-%x_f = [ ones(1,o)*x_o x_f];
-%y_f = [ ones(1,o)*y_o y_f];
-
-%clear x_o y_o o
-% create an array with the power.
-%a_f = aom_t*ones(1,length(x_f));
-
-%
-
-
-
-[x_s,y_s] = meshgrid(-1:0.5:1,-1:0.5:1);
+[x_s,y_s] = meshgrid(-1:stp:1,-1:stp:1);
 x_s=reshape(x_s,1,[]);
 y_s=reshape(y_s,1,[]);
-x_f=reshape(ones(10,1)*x_s,1,[]);
-y_f=reshape(ones(10,1)*y_s,1,[]);
+x_f=reshape(ones(number_of_point,1)*x_s,1,[]);
+y_f=reshape(ones(number_of_point,1)*y_s,1,[]);
 clear x_s y_s;
 a_f = aom_t*ones(1,length(x_f));
 clear aom_t
